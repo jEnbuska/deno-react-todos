f02560f05338ecb50587bf145abe7eeb8c05a5ff
joona enbuska <joonaenb@gmail.com>
2022-09-22 15:23:23 +0300

    add multi client support

--- main.ts
+++ main.ts
@@ -6,6 +6,38 @@ serve(handler, {port: 4242});
 
 const todos: Todo[] = [];
 
+let sockets: WebSocket[] = [];
+
+const send = (message: TodosClientMessage) => sockets.forEach(s => s.send(JSON.stringify(message)));
+
+const onMessage = (e: MessageEvent) => {
+	const message: TodosServerMessage = JSON.parse(e.data)
+	switch (message.type) {
+		case 'ADD':
+			const todo: Todo = {id: Math.random().toString(36).substr(2, 9), title: message.payload, done: false}
+			todos.push(todo);
+			send({type: 'ADD', payload: todo});
+			break;
+		case 'REMOVE': {
+			const index = todos.findIndex(todo => todo.id === message.payload);
+			if (index !== -1) {
+				todos.splice(index, 1);
+				send({type: 'REMOVE', payload: message.payload});
+			}
+			break;
+		}
+		case 'TOGGLE': {
+			const todo = todos.find(todo => todo.id === message.payload);
+			if (todo) {
+				todo.done = !todo.done;
+				send({type: 'UPDATE', payload: todo});
+			}
+			break;
+		}
+	}
+	;
+}
+
 function handler(req: Request): Response {
 	const upgrade = req.headers.get("upgrade") || "";
 	let response, socket: WebSocket;
@@ -14,36 +46,15 @@ function handler(req: Request): Response {
 	} catch {
 		return new Response("request isn't trying to upgrade to websocket.");
 	}
-	const send = (message: TodosClientMessage) => socket.send(JSON.stringify(message));
+	
 	socket.onopen = () => {
+		sockets = [...sockets, socket];
 		send({type: 'INIT', payload: todos});
 	};
-	socket.onmessage = (e) => {
-		const message: TodosServerMessage = JSON.parse(e.data)
-		switch (message.type) {
-			case 'ADD':
-				const todo: Todo = {id: Math.random().toString(36).substr(2, 9), title: message.payload, done: false}
-				todos.push(todo);
-				send({type: 'ADD', payload: todo});
-				break;
-			case 'REMOVE': {
-				const index = todos.findIndex(todo => todo.id === message.payload);
-				if (index !== -1) {
-					todos.splice(index, 1);
-					send({type: 'REMOVE', payload: message.payload});
-				}
-				break;
-			}
-			case 'TOGGLE': {
-				const todo = todos.find(todo => todo.id === message.payload);
-				if (todo) {
-					todo.done = !todo.done;
-					send({type: 'UPDATE', payload: todo});
-				}
-				break;
-			}
-		}
-	};
+	socket.onclose = () => {
+		sockets = sockets.filter(s => s !== socket);
+	}
+	socket.onmessage = onMessage
 	
 	socket.onerror = (e) => console.log("socket errored:", e);
 	socket.onclose = () => console.log("socket closed");
